cdd840d9bc237aa64e362bbe0a786b1a00f0351e
dev-harsh1998 <harshit.india@linaro.org>
2018-01-08 00:06:35 +0530

    a6000-64: camera: Go Prebuilt

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -2,8 +2,6 @@
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl.a6000 \
     camera.device@1.0-impl.a6000 \
-    camera.msm8916 \
-    libmm-qcamera \
     libshim_atomic \
     Snap
 
@@ -15,6 +13,18 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
+
+# Prebuilt Cam blobs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/camera/camera.msm8916.so:system/lib/hw/camera.msm8916.so \
+    $(LOCAL_PATH)/camera/libcamera2ndk.so:system/lib/libcamera2ndk.so \
+    $(LOCAL_PATH)/camera/libcamera_client.so:system/lib/libcamera_client.so \
+    $(LOCAL_PATH)/camera/libcamera_metadata.so:system/lib/libcamera_metadata.so \
+    $(LOCAL_PATH)/camera/libcameraservice.so:system/lib/libcameraservice.so \
+    $(LOCAL_PATH)/camera/libmmcamera_interface.so:system/lib/libmmcamera_interface.so \
+    $(LOCAL_PATH)/camera/libmmjpeg_interface.so:system/lib/libmmjpeg_interface.so \
+    $(LOCAL_PATH)/camera/libqomx_core.so:system/lib/libqomx_core.so \
+    $(LOCAL_PATH)/camera/libmm-qcamera.so:system/lib/libmm-qcamera.so
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
